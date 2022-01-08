7c362adc4ef96afffcd451ee4aa456121c2db532
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-01-08 21:39:24 +0700

    EditText

--- app/src/main/java/com/example/android_fundamentals/MainActivity.kt
+++ app/src/main/java/com/example/android_fundamentals/MainActivity.kt
@@ -2,19 +2,19 @@ package com.example.android_fundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var count = 0
-        btnCount.setOnClickListener{
-            count++
-            tvCount.text = "Let's, Count Together= $count"
+        btnAdd.setOnClickListener{
+            val firstNumber = etFirstNumber.text.toString().toInt()
+            val secondNumber = etSecondNumber.text.toString().toInt()
+            val result = firstNumber + secondNumber
+
+            tvResult.text = result.toString()
         }
     }
 }
