ded5867beafde0f260a172cf4eb507cfa9e5c24d
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-01-08 19:34:14 +0700

    Buttons

--- app/src/main/java/com/example/android_fundamentals/MainActivity.kt
+++ app/src/main/java/com/example/android_fundamentals/MainActivity.kt
@@ -2,19 +2,22 @@ package com.example.android_fundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onPause() {
-        super.onPause()
-        println("onPause")
-    }
+        btnApply.setOnClickListener {
+            val firsName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val birthDate = etBirthDate.text.toString()
+            val country = etCountry.text.toString()
 
-    override fun onRestart() {
-        super.onRestart()
+            Log.d("MainActivity", "$firsName $lastName born on $birthDate, from $country applied to the form")
+        }
     }
 }
