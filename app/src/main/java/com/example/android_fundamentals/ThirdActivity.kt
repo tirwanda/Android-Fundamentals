11789c09b84721420e449131594a3cd63733831c
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-01-11 14:30:21 +0700

    Intents and Starting Activities

--- app/src/main/java/com/example/android_fundamentals/ThirdActivity.kt
+++ app/src/main/java/com/example/android_fundamentals/ThirdActivity.kt
@@ -3,18 +3,21 @@ package com.example.android_fundamentals
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_third.*
 
-class MainActivity : AppCompatActivity() {
+class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_third)
 
-        btnOpenActivity.setOnClickListener {
+        btnGoToSecondActivity.setOnClickListener {
             Intent(this, SecondActivity::class.java).also {
                 startActivity(it)
             }
         }
 
+        btnHome.setOnClickListener {
+            finish()
+        }
     }
 }
