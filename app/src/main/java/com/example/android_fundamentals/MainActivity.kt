0e0f5efca52746cf1a4c7197dc3bc89ef0965e99
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-01-13 14:43:22 +0700

    Permissions

--- app/src/main/java/com/example/android_fundamentals/MainActivity.kt
+++ app/src/main/java/com/example/android_fundamentals/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android_fundamentals
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -27,5 +30,58 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        btnPermission.setOnClickListener {
+            requestPermissions()
+        }
+    }
+
+    private fun hasWriteExternalStoragePermission() =
+        ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun hasLocationForegroundPermission() =
+        ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun hasLocationBackgroundPermission() =
+        ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun requestPermissions() {
+        var permissionToRequest = mutableListOf<String>()
+
+        if (!hasWriteExternalStoragePermission()) {
+            permissionToRequest.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+        if (!hasLocationForegroundPermission()) {
+            permissionToRequest.add(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+
+        if (!hasLocationBackgroundPermission()) {
+            permissionToRequest.add(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+
+        if (permissionToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionToRequest.toTypedArray(), 0)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0 && grantResults.isNotEmpty()) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("PermissionsRequest: ", "${permissions[i]} granted")
+                }
+            }
+        }
     }
 }
