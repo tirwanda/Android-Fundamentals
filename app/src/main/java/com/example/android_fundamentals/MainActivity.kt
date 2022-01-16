ba35d947588959e1b30d90293da47df79ae2904f
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-01-16 15:49:06 +0700

    Alert Dialog

--- app/src/main/java/com/example/android_fundamentals/MainActivity.kt
+++ app/src/main/java/com/example/android_fundamentals/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.example.android_fundamentals
 
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,21 +11,56 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
+        val addContactDialog = AlertDialog.Builder(this)
+            .setTitle("Add Contact")
+            .setMessage("Do you want to add Mr. Edho to your contact list?")
+            .setIcon(R.drawable.ic_add_contact)
+            .setPositiveButton("Yes") { _, _ ->
+                Toast.makeText(this, "You Added mr. Edho to your contact list", Toast.LENGTH_LONG).show()
+            }
+            .setNegativeButton("No") { _, _ ->
+                Toast.makeText(this, "You didn't add mr. Edho to your contact list", Toast.LENGTH_LONG).show()
+            }.create()
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.app_bar_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.miAddContact -> Toast.makeText(this, "Add Contact is Clicked", Toast.LENGTH_SHORT).show()
-            R.id.miFavorites -> Toast.makeText(this, "Favorites is Clicked", Toast.LENGTH_SHORT).show()
-            R.id.miSettings -> Toast.makeText(this, "Settings is Clicked", Toast.LENGTH_SHORT).show()
-            R.id.miFeedback -> Toast.makeText(this, "Give Feedback is Clicked", Toast.LENGTH_SHORT).show()
-            R.id.miClose -> finish()
+        btnDialog1.setOnClickListener {
+            addContactDialog.show()
         }
-        return true
+
+        val options = arrayOf("First Item", "Second Item", "Third Item")
+        val singleChoiceDialog = AlertDialog.Builder(this)
+            .setTitle("Choice one of these options")
+            .setSingleChoiceItems(options, 0) { dialogInterface, i ->
+                Toast.makeText(this, "You click on ${options[i]}", Toast.LENGTH_SHORT).show()
+            }
+            .setPositiveButton("Accept") { _, _ ->
+                Toast.makeText(this, "You accepted the SingleChoiceDialog", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Decline") { _, _ ->
+                Toast.makeText(this, "You Decline the SingleChoiceDialog", Toast.LENGTH_SHORT).show()
+            }.create()
+
+        btnDialog2.setOnClickListener {
+            singleChoiceDialog.show()
+        }
+
+        val multipleChoiceDialog = AlertDialog.Builder(this)
+            .setTitle("Multiple Choice Dialog")
+            .setMultiChoiceItems(options, booleanArrayOf(false, false, false)) { _, i, isChecked ->
+                if (isChecked) {
+                    Toast.makeText(this, "You Checked ${options[i]}", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "You Uncheck ${options[i]}", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .setPositiveButton("Accept") { _, _ ->
+                Toast.makeText(this, "You accepted the multiple choice dialog", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Decline") { _, _ ->
+                Toast.makeText(this, "You decline the multiple choice dialog", Toast.LENGTH_SHORT).show()
+            }.create()
+
+        btnDialog3.setOnClickListener {
+            multipleChoiceDialog.show()
+        }
     }
 }
