90ac0858139d757fc26763878446d3568430f895
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-01-21 23:05:16 +0700

    Notifications

--- app/src/main/java/com/example/android_fundamentals/MainActivity.kt
+++ app/src/main/java/com/example/android_fundamentals/MainActivity.kt
@@ -1,46 +1,61 @@
 package com.example.android_fundamentals
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val CHANNEL_ID = "channelID"
+    val CHANNEL_NAME = "channelName"
+    val NOTIFICATION_ID = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotificationChannel()
 
-        val sharedPref = this.getSharedPreferences("myPref", Context.MODE_PRIVATE)
-        val editor = sharedPref.edit()
-
-        btnSave.setOnClickListener {
-            val name = etName.text.toString()
-            val age = etAge.text.toString().toInt()
-            val isAdult = cbAdult.isChecked
-
-//            editor.apply{
-//                putString("name", name)
-//                putInt("age", age)
-//                putBoolean("isAdult", isAdult)
-//                apply()
-//            }
-
-            editor.putString("name", name)
-            editor.putInt("age", age)
-            editor.putBoolean("isAdult", isAdult)
-            editor.apply()
-
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
         }
 
-        btnLoad.setOnClickListener {
-            val name = sharedPref.getString("name", null)
-            val age = sharedPref.getInt("age", 0)
-            val isAdult = sharedPref.getBoolean("isAdult", false)
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("My Notification")
+            .setContentText("This is a content of Notification")
+            .setSmallIcon(R.drawable.ic_notification)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
 
-            etName.setText(name)
-            etAge.setText(age)
-            cbAdult.isChecked = isAdult
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        btnNotification.setOnClickListener {
+            notificationManager.notify(NOTIFICATION_ID, notification)
+        }
+    }
+
+    fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
         }
     }
 }
